8efd5f1a9527cece39c30e0a501216cc8b9e7fe4
Tommaso Omiccioli <tommaso omiccioli@pc-tommaso.homenet.telecomitalia.it>
2020-12-16 18:39:57 +0100

    Riadattamento classe Statistiche

--- ProgettoOOPMeteo/src/main/java/it/univpm/ProgettoOOPMeteo/Operazioni/Statistiche.java
+++ ProgettoOOPMeteo/src/main/java/it/univpm/ProgettoOOPMeteo/Operazioni/Statistiche.java
@@ -2,52 +2,102 @@ package it.univpm.ProgettoOOPMeteo.Operazioni;
 
 import citta.Citta;
 
+/**
+ * Questa classe ha lo scopo di implementare i metodi che deterimnano la media e la varianza per il vento e la visibilità 
+ * della citta che viene passata come attributo di tutti i metodi nel lasso di tempo compreso tra i valori di inizio e fine
+ * 
+ * @author Nicola Ronchini, Tommaso Omiccioli
+ *
+ */
 public class Statistiche {
-
+	
+	/**
+	 * Costruttore di Default senza parametri inclusi
+	 */
+	 public Statistiche() {}
 	 
-	public Statistiche() {}
-	 
-
-   public float mediaVento (Citta cit){
-       
-       float somma=0;
-       for (int i=0; i<cit.GetSizeVento() ; i++) {
-           somma += cit.getPosVelocita(i);
-       }
-       return somma/cit.GetSizeVento();
-       
-   }
-   
-   public float mediaVis (Citta cit){
-       
-       float somma=0;
-       for (int i=0; i < cit.GetSizeVis() ; i++) {
-           somma += cit.getPosVisibilita(i);
-       }
-       return somma/cit.GetSizeVis();
-   }
-   
-   
-   public float varianzaVento (Citta cit) {
-   
-   float somma=0; 
-   float media = mediaVento(cit);
-   
-   for (int i=0; i<cit.GetSizeVento(); i++) {
-       somma += (cit.getPosVelocita(i) - media)*(cit.getPosVelocita(i) - media);
-       }
-   return somma/(cit.GetSizeVento() - 1);
+	
+	 /**
+	  * Questo metodo si occupa di calcolare la media del vento tra un rilevazione di inizio e una di fine
+	  * 
+	  * @param citta
+	  * @param inizio
+	  * @param fine
+	  * @return mediaVento
+	  */
+	 public float mediaVento (Citta citta, int inizio, int fine){
+		
+			float somma=0;
+		    
+			for (int i=inizio; i<=fine ; i++) {
+		    	somma += citta.getPosVelocita(i);
+		    }
+			return somma/(fine - inizio);
+		}
+	
+	 /**
+	  * Questo metodo si occupa di calcolare la media della visibilita tra un rilevazione di inizio e una di fine
+	  * 
+	  * @param citta
+	  * @param inizio
+	  * @param fine
+	  * @return mediaVisibilita
+	  */
+	
+	 public float mediaVisibilita (Citta citta, int inizio, int fine){
+		
+	        float somma=0;
+		    
+			for (int i=inizio; i<=fine ; i++) {
+		    	somma += citta.getPosVisibilita(i);
+		    }
+			return somma/(fine - inizio);
+	}
+		
+	
+	 /**
+	 * Questo metodo si occupa di calcolare la varianza della velocita del vento tra un rilevazione di inizio e una di fine
+	 * 
+	 * @param citta
+	 * @param inizio
+	 * @param fine
+	 * @return varianzaVento
+	 */
+    public float varianzaVento (Citta citta, int inizio, int fine) {
+	
+	float somma=0; 
+	
+	float media = mediaVento(citta, inizio, fine);
+	
+	for (int i=inizio; i<=fine; i++) {
+		somma += (citta.getPosVelocita(i) - media)*(citta.getPosVelocita(i) - media);
+		}
+	return somma/(fine - inizio);
 }
-   
-   public float varianzaVis (Citta cit) {
-   
-   float somma=0; 
-   float media = mediaVis(cit);
-   
-   for (int i=0; i<cit.GetSizeVis(); i++) {
-       somma += (cit.getPosVisibilita(i) - media)*(cit.getPosVisibilita(i) - media);
-       }
-   return somma/(cit.GetSizeVis() - 1);
-   }
-   
+	
+
+	 
+	 /**
+	 * Questo metodo si occupa di calcolare la varianza della visibilita tra un rilevazione di inizio e una di fine
+	 * 
+	 * @param citta
+	 * @param inizio
+	 * @param fine
+	 * @return varianzaVisibilita
+	 */
+   public float varianzaVisibilita (Citta citta, int inizio, int fine) {
+	
+	float somma=0; 
+	
+	float media = mediaVento(citta, inizio, fine);
+	
+	for (int i=inizio; i<=fine; i++) {
+		somma += (citta.getPosVisibilita(i) - media)*(citta.getPosVisibilita(i) - media);
+		}
+	return somma/(fine - inizio);
+}
+	 
+
+
+
 }
