f9720d51eb01e4fcfdaf90150934a0df1427deba
Nicola Ronchini <s1092303@studenti.univpm.it>
2020-12-18 10:19:50 +0100

    rotta2

--- ProgettoOOPMeteo/src/main/java/it/univpm/ProgettoOOPMeteo/Controller/ControllerMeteo.java
+++ ProgettoOOPMeteo/src/main/java/it/univpm/ProgettoOOPMeteo/Controller/ControllerMeteo.java
@@ -4,6 +4,7 @@ import java.util.Vector;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import Filtri.FiltroPerNome;
@@ -28,7 +29,7 @@ public class ControllerMeteo {
 	}
 	
 	@PostMapping("/nomeCitta")
-	public Citta method() {
+	public Citta method(@RequestParam(name = "param") String param) {
 		FiltroPerNome f = new FiltroPerNome("Roma");
 		return f.getCitta();
 	}
