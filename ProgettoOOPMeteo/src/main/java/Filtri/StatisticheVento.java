dda79467f9145927ba6f55c85ac51e6bf436e9cd
Tommaso Omiccioli <tommaso omiccioli@pc-tommaso.homenet.telecomitalia.it>
2020-12-29 11:13:21 +0100

    Javadoc Statistiche vento e visibilita

--- ProgettoOOPMeteo/src/main/java/Filtri/StatisticheVento.java
+++ ProgettoOOPMeteo/src/main/java/Filtri/StatisticheVento.java
@@ -7,91 +7,134 @@ import java.util.Vector;
 import org.json.simple.parser.ParseException;
 
 import citta.CittaMediaVar;
-
+/**
+ * Queta classe permette di raccogliere le citta con velocita del vento media massima e minima e con varianza massiam e minima in un intervallo di tempo
+ * determinato. 
+ * 
+ * @author Nicola Ronchini, tommaso omiccioli
+ *
+ */
 public class StatisticheVento {
 
 	private Vector<CittaMediaVar> MaxMediaVento = new Vector<CittaMediaVar> ();             //vettore con la citta con la media più alta o le citta che hanno pari merito media più alta
-	private Vector<CittaMediaVar> MinMediaVento = new Vector<CittaMediaVar> ();
-	private Vector<CittaMediaVar> MaxVarianzaVento = new Vector<CittaMediaVar> ();
-	private Vector<CittaMediaVar> MinVarianzaVento = new Vector<CittaMediaVar> ();
-	private Vector<CittaMediaVar> ListaCitta = new Vector<CittaMediaVar>();
+	private Vector<CittaMediaVar> MinMediaVento = new Vector<CittaMediaVar> ();             //vettore con la citta con la media più bassa o le citta che hanno pari merito media più bassa
+	private Vector<CittaMediaVar> MaxVarianzaVento = new Vector<CittaMediaVar> ();          //vettore con la citta con la varianza più alta o le citta che hanno pari merito varianza più alta
+	private Vector<CittaMediaVar> MinVarianzaVento = new Vector<CittaMediaVar> ();          //vettore con la citta con la varianza piu bassa o le citta che hanno pari merito varianza piu bassa
+	private Vector<CittaMediaVar> ListaCitta = new Vector<CittaMediaVar>();                 //lista delle citta 
 	
 	private int inizio;
 	private int fine;
 	
-	
+	/**
+	 * 
+	 * Il costruttore, passatogli a gli istanti di inizio e di fine del periodo da analizzare, riempe i Vector della media massima
+	 * e minima e della varianza massimae minima
+	 * 
+	 * @param inizio
+	 * @param fine
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 * @throws ParseException
+	 */
 	public StatisticheVento (int inizio, int fine) throws FileNotFoundException, IOException, ParseException {
 	
 		this.inizio = inizio;
 		this.fine = fine;
 		
-		StatisticheAScelta s = new StatisticheAScelta (inizio, fine);
-		ListaCitta = s.getVettore();
+		StatisticheAScelta s = new StatisticheAScelta (inizio, fine);         
+		ListaCitta = s.getVettore();                             //inizzializziamo il vettore di CittaMediaVar con le medie e varianze di tutte le citta
 		
-		MaxMediaVento.add(0, ListaCitta.get(0));
-		MinMediaVento.add(0,  ListaCitta.get(0));
-		MaxVarianzaVento.add(0, ListaCitta.get(0));
-		MinVarianzaVento.add(0, ListaCitta.get(0));
+		MaxMediaVento.add(0, ListaCitta.get(0));                 //aggiungiamo nel vettore MaxMediaVento la prima citta della listaCitta
+		MinMediaVento.add(0,  ListaCitta.get(0));                //aggiungiamo nel vettore MinMediaVento la prima citta della listaCitta
+		MaxVarianzaVento.add(0, ListaCitta.get(0));              //aggiungiamo nel vettore MaxVarianzaVento la prima citta della listaCitta
+		MinVarianzaVento.add(0, ListaCitta.get(0));              //aggiungiamo nel vettore MinVarianzaVento la prima citta della listaCitta
 		
-		for (int i = 1; i<ListaCitta.size(); i++ ) {
+		for (int i = 1; i<ListaCitta.size(); i++ ) {             //il for si occupa di far confrontare ogni citta con i vettori di MaxMediaVento, MinMediaVento, MaxVarianzaVento, MinVarianzaVento
 			
-			if(ListaCitta.get(i).getMediaVento() > MaxMediaVento.get(0).getMediaVento()) { MaxMediaVento.removeAllElements();
-				                                                                            MaxMediaVento.add(0, ListaCitta.get(i));}
-			else if(ListaCitta.get(i).getMediaVento() == MaxMediaVento.get(0).getMediaVento()) {MaxMediaVento.add(MaxMediaVento.size(), ListaCitta.get(i));}
+			if(ListaCitta.get(i).getMediaVento() > MaxMediaVento.get(0).getMediaVento()) { MaxMediaVento.removeAllElements();                                 //controllla se la media della citta i-esima è maggiore della citta che fino ad ora ha avuto la media massima
+				                                                                            MaxMediaVento.add(0, ListaCitta.get(i));}                         // se cosi fosse svuota tutto il vettore MaxMediaVento e lo inizzializza con queta nuova citta
+			else if(ListaCitta.get(i).getMediaVento() == MaxMediaVento.get(0).getMediaVento()) {MaxMediaVento.add(MaxMediaVento.size(), ListaCitta.get(i));}  //se invece la media della citta i-esima è uguale a quella della citta massima allora aggiunge anche la prima al vettore MaxMediaVento 
 			
 			
-			if(ListaCitta.get(i).getMediaVento() < MinMediaVento.get(0).getMediaVento()) { MinMediaVento.removeAllElements();
-                                                                                            MinMediaVento.add(0, ListaCitta.get(i));}
-			else if(ListaCitta.get(i).getMediaVento() == MinMediaVento.get(0).getMediaVento()) {MinMediaVento.add(MinMediaVento.size(), ListaCitta.get(i));}	
+			if(ListaCitta.get(i).getMediaVento() < MinMediaVento.get(0).getMediaVento()) { MinMediaVento.removeAllElements();                                 //controllla se la media della citta i-esima è minore della citta che fino ad ora ha avuto la media minima  
+                                                                                            MinMediaVento.add(0, ListaCitta.get(i));}                         // se cosi fosse svuota tutto il vettore MinMediaVento e lo inizzializza con queta nuova citta
+			else if(ListaCitta.get(i).getMediaVento() == MinMediaVento.get(0).getMediaVento()) {MinMediaVento.add(MinMediaVento.size(), ListaCitta.get(i));}  //se invece la media della citta i-esima è uguale a quella della citta minima allora aggiunge anche la prima al vettore MinMediaVento 
+			
 			
 		    
-            if(ListaCitta.get(i).getVarianzaVento() > MaxVarianzaVento.get(0).getVarianzaVento()) { MaxVarianzaVento.removeAllElements();
-                                                                                                     MaxVarianzaVento.add(0, ListaCitta.get(i));}
-            else if(ListaCitta.get(i).getVarianzaVento() == MaxVarianzaVento.get(0).getVarianzaVento()){ MaxVarianzaVento.add(MaxVarianzaVento.size(), ListaCitta.get(i));}
+            if(ListaCitta.get(i).getVarianzaVento() > MaxVarianzaVento.get(0).getVarianzaVento()) { MaxVarianzaVento.removeAllElements();                                   //controllla se la varianza della citta i-esima è maggiore della citta che fino ad ora ha avuto la varianza massima
+                                                                                                     MaxVarianzaVento.add(0, ListaCitta.get(i));}                           // se cosi fosse svuota tutto il vettore MaxVarianzaVento e lo inizzializza con queta nuova citta
+            else if(ListaCitta.get(i).getVarianzaVento() == MaxVarianzaVento.get(0).getVarianzaVento()){ MaxVarianzaVento.add(MaxVarianzaVento.size(), ListaCitta.get(i));} //se invece la varianza della citta i-esima è uguale a quella della citta massima allora aggiunge anche la prima al vettore MaxVarianzaVento 
+			
             
-            if (ListaCitta.get(i).getVarianzaVento() < MinVarianzaVento.get(0).getVarianzaVento()) { MinVarianzaVento.removeAllElements();
-            																						 MinVarianzaVento.add(0, ListaCitta.get(i)); }
-            else if(ListaCitta.get(i).getVarianzaVento() == MinVarianzaVento.get(0).getVarianzaVento()) { MinVarianzaVento.add(MinVarianzaVento.size(), ListaCitta.get(i));}
+            if (ListaCitta.get(i).getVarianzaVento() < MinVarianzaVento.get(0).getVarianzaVento()) { MinVarianzaVento.removeAllElements();                                  //controllla se la varianza della citta i-esima è minore della citta che fino ad ora ha avuto la varianza minima
+            																						 MinVarianzaVento.add(0, ListaCitta.get(i)); }                          // se cosi fosse svuota tutto il vettore MinVarianzaVento e lo inizzializza con queta nuova citta
+            else if(ListaCitta.get(i).getVarianzaVento() == MinVarianzaVento.get(0).getVarianzaVento()) { MinVarianzaVento.add(MinVarianzaVento.size(), ListaCitta.get(i));}//se invece la varianza della citta i-esima è uguale a quella della citta minima allora aggiunge anche la prima al vettore MinVarianzaVento 
+			
             }
 		
 		}
 
-
+/**
+ * Ritorna il vettore di CittaMediaVento che rappresentano le citta piu ventose
+ * @return vettore CittaMediaVar
+ */
 	public Vector<CittaMediaVar> getMaxMediaVento() {
 		return MaxMediaVento;
 	}
 
-
+/**
+ * Contente di modificare il vettore MaxMediaVento
+ * @param maxMediaVento
+ */
 	public void setMaxMediaVento(Vector<CittaMediaVar> maxMediaVento) {
 		MaxMediaVento = maxMediaVento;
 	}
 
-
+	/**
+	 * Ritorna il vettore di CittaMediaVento che rappresentano le citta meno ventose
+	 * @return vettore CittaMediaVar
+	 */
 	public Vector<CittaMediaVar> getMinMediaVento() {
 		return MinMediaVento;
 	}
 
-
+	/**
+	 * Contente di modificare il vettore MinMediaVento
+	 * @param maxMediaVento
+	 */
 	public void setMinMediaVento(Vector<CittaMediaVar> minMediaVento) {
 		MinMediaVento = minMediaVento;
 	}
 
-
+	/**
+	 * Ritorna il vettore di CittaMediaVento che rappresentano le citta con varianza piu alta
+	 * @return vettore CittaMediaVar
+	 */
 	public Vector<CittaMediaVar> getMaxVarianzaVento() {
 		return MaxVarianzaVento;
 	}
 
-
+	/**
+	 * Contente di modificare il vettore MaxVarianzaVento
+	 * @param maxMediaVento
+	 */
 	public void setMaxVarianzaVento(Vector<CittaMediaVar> maxVarianzaVento) {
 		MaxVarianzaVento = maxVarianzaVento;
 	}
 
-
+	/**
+	 * Ritorna il vettore di CittaMediaVento che rappresentano le citta con varianza piu bassa
+	 * @return vettore CittaMediaVar
+	 */
 	public Vector<CittaMediaVar> getMinVarianzaVento() {
 		return MinVarianzaVento;
 	}
 
-
+	/**
+	 * Contente di modificare il vettore MinVarianzaVento
+	 * @param maxMediaVento
+	 */
 	public void setMinVarianzaVento(Vector<CittaMediaVar> minVarianzaVento) {
 		MinVarianzaVento = minVarianzaVento;
 	}
--- ProgettoOOPMeteo/src/main/java/Filtri/StatisticheVisibilita.java
+++ ProgettoOOPMeteo/src/main/java/Filtri/StatisticheVisibilita.java
@@ -7,33 +7,51 @@ import java.util.Vector;
 import org.json.simple.parser.ParseException;
 
 import citta.CittaMediaVar;
-
+/**
+ * Queta classe permette di raccogliere le citta con visibilita media massima e minima e con varianza massiam e minima in un intervallo di tempo
+ * determinato. 
+ * 
+ * @author Nicola Ronchini, tommaso omiccioli
+ *
+ */
 public class StatisticheVisibilita {
 
-	private Vector<CittaMediaVar> MaxMediaVisibilita = new Vector<CittaMediaVar> ();  
-	private Vector<CittaMediaVar> MinMediaVisibilita = new Vector<CittaMediaVar> ();
-	private Vector<CittaMediaVar> MaxVarianzaVisibilita  = new Vector<CittaMediaVar> ();
-	private Vector<CittaMediaVar> MinVarianzaVisibilita  = new Vector<CittaMediaVar> ();
-	private Vector<CittaMediaVar> ListaCitta = new Vector<CittaMediaVar>();
+	private Vector<CittaMediaVar> MaxMediaVisibilita = new Vector<CittaMediaVar> ();        //vettore con la citta con la media più alta o le citta che hanno pari merito media più alta
+	private Vector<CittaMediaVar> MinMediaVisibilita = new Vector<CittaMediaVar> ();        //vettore con la citta con la media più bassa o le citta che hanno pari merito media più bassa
+	private Vector<CittaMediaVar> MaxVarianzaVisibilita  = new Vector<CittaMediaVar> ();    //vettore con la citta con la varianza più alta o le citta che hanno pari merito varianza più alta
+	private Vector<CittaMediaVar> MinVarianzaVisibilita  = new Vector<CittaMediaVar> ();    //vettore con la citta con la varianza piu bassa o le citta che hanno pari merito varianza piu bassa
+	private Vector<CittaMediaVar> ListaCitta = new Vector<CittaMediaVar>();                 //lista delle citta
 	
 	private int inizio;
 	private int fine;
 	
-	
+	/**
+	 * 
+	 * Il costruttore, passatogli a gli istanti di inizio e di fine del periodo da analizzare, riempe i Vector della media massima
+	 * e minima e della varianza massimae minima
+	 * 
+	 * @param inizio
+	 * @param fine
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 * @throws ParseException
+	 */
 	public StatisticheVisibilita (int inizio, int fine) throws FileNotFoundException, IOException, ParseException {
 	
 		this.inizio = inizio;
 		this.fine = fine;
 		
 		StatisticheAScelta s = new StatisticheAScelta (inizio, fine);
-		ListaCitta = s.getVettore();
+		ListaCitta = s.getVettore();                                   //inizzializziamo il vettore di CittaMediaVar con le medie e varianze di tutte le citta
 		
-		MaxMediaVisibilita .add(0, ListaCitta.get(0));
-		MinMediaVisibilita .add(0,  ListaCitta.get(0));
-		MaxVarianzaVisibilita .add(0, ListaCitta.get(0));
-		MinVarianzaVisibilita .add(0, ListaCitta.get(0));
+		MaxMediaVisibilita .add(0, ListaCitta.get(0));     //aggiungiamo nel vettore MaxMediaVisibilita la prima citta della listaCitta
+		MinMediaVisibilita .add(0,  ListaCitta.get(0));    //aggiungiamo nel vettore MinMediaVisibilita la prima citta della listaCitta
+		MaxVarianzaVisibilita .add(0, ListaCitta.get(0));  //aggiungiamo nel vettore MaxVarianzaVisibilita la prima citta della listaCitta
+		MinVarianzaVisibilita .add(0, ListaCitta.get(0));  //aggiungiamo nel vettore MinVarianzaVisibilita la prima citta della listaCitta
 		
-		for (int i = 1; i<ListaCitta.size(); i++ ) {
+		
+		for (int i = 1; i<ListaCitta.size(); i++ ) {             //il for si occupa di far confrontare ogni citta con i vettori di MaxMediaVisibilita, MinMediaVisibilita, MaxVarianzaVento, MinVarianzaVento
+			
 			
 			if(ListaCitta.get(i).getMediaVisibilita () > MaxMediaVisibilita .get(0).getMediaVisibilita ()) { MaxMediaVisibilita .removeAllElements();
 				                                                                            MaxMediaVisibilita .add(0, ListaCitta.get(i));}
